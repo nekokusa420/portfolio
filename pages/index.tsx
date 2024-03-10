a95ba84ed741a0a15f736411a9ab678f97e84fb1
nekokusa420 <sk011010ab@gmail.com>
2024-03-11 01:08:47 +0800

    98

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
-        <title>Kit's portfolio123</title>
+        <title>Kit's portfolio</title>
       </Head>
       {/* <h1 className="text-red-800">Lets build and AWESOME portfolio</h1> */}
 
