e6198fb15e379318d2de98c4a8a071bd73bcda6c
nekokusa420 <sk011010ab@gmail.com>
2024-03-11 00:50:07 +0800

    98

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,7 +12,6 @@ export default function Hero({}: Props) {
   });
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
-      {" "}
       <BackgroundCircles />
       <h1>
         <span>{text}</span>
