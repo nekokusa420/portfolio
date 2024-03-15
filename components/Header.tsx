4165cb7ebda9443c0ccc4d6a35e522332e778052
nekokusa420 <sk011010ab@gmail.com>
2024-03-16 07:10:48 +0800

    98

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 type Props = {};
 
 export default function Header({}: Props) {
+  const { push } = useRouter();
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
       <motion.div
@@ -55,33 +57,36 @@ export default function Header({}: Props) {
           bgColor="transparent"
         />
       </motion.div>
-      <Link href="#contact">
-        <motion.div
-          initial={{
-            x: -500,
-            opacity: 0,
-            scale: 0.5,
-          }}
-          animate={{
-            x: 0,
-            opacity: 1,
-            scale: 1,
-          }}
-          transition={{
-            duration: 1.5,
-          }}
-        >
-          <SocialIcon
-            className="flex flex-row items-center text-gray-300 cursor-pointer"
-            network="email"
-            fgColor="grey"
-            bgColor="transparent"
-          />
-          <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
-            Get In Touch
-          </p>
-        </motion.div>
-      </Link>
+
+      <motion.div
+        className="cursor-pointer"
+        onClick={() => {
+          push("#contact");
+        }}
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+      >
+        <SocialIcon
+          className="flex flex-row items-center text-gray-300 cursor-pointer"
+          network="email"
+          fgColor="grey"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Get In Touch
+        </p>
+      </motion.div>
     </header>
   );
 }
